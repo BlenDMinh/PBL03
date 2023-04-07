f60364dadd53f4ee5aafaba429ddc8d034d9f8a7
hieupt <trunghieupham03@gmail.com>
2023-04-07 16:45:23 +0700

    Fix client bugs

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import logo from "public/logo.png";
-import { BsCartPlus, BsFillPersonFill } from "react-icons/bs";
-import { BsSearch } from "react-icons/bs";
+import { BsCartPlus, BsFillPersonFill, BsSearch } from "react-icons/bs";
 import { AiOutlineDown } from "react-icons/ai";
+import Logo from "public/Logo.png";
 
 export default function Navbar() {
   const categories = [
@@ -31,68 +30,64 @@ export default function Navbar() {
   const router = useRouter();
 
   return (
-    <>
-      <main>
-        <header className="bg-[#ed1c24] text-base font-semibold border-none outline-none">
-          <nav
-            className="mx-auto flex max-w-6xl items-center justify-between"
-            aria-label="Global"
-          >
-            <a onClick={() => router.push("/")}>
-              <Image src={logo} alt="WinMart logo" width={175} />
+    <header className="bg-[#ed1c24] text-base font-semibold border-none outline-none">
+      <nav
+        className="mx-auto flex max-w-6xl items-center justify-between"
+        aria-label="Global"
+      >
+        <a onClick={() => router.push("/")}>
+          <Image src={Logo} alt="WinMart Logo" width={175} />
+        </a>
+
+        <div className="flex bg-white rounded-xl">
+          <div className="relative border-r-2 group/item">
+            <a
+              href="#"
+              className="text-sm flex items-center justify-center p-3 w-52"
+            >
+              Danh mục sản phẩm
+              <AiOutlineDown className="ml-1 text-xs" />
             </a>
-
-            <div className="flex bg-white rounded-xl">
-              <div className="relative border-r-2 group/item">
-                <a
-                  href="#"
-                  className="text-sm flex items-center justify-center p-3 w-52"
-                >
-                  Danh mục sản phẩm
-                  <AiOutlineDown className="ml-1 text-xs" />
-                </a>
-                <div className="invisible group-hover/item:visible -mt-3 pt-3 rounded-b-lg overflow-hidden absolute z-10 shadow-lg w-52 bg-white py-2">
-                  {categories.map((value, index) => {
-                    return (
-                      <a
-                        href="#"
-                        key={index}
-                        className="text-sm font-medium block px-3.5 py-1.5 hover:bg-[#ed1c24] hover:text-white"
-                      >
-                        {value}
-                      </a>
-                    );
-                  })}
-                </div>
-              </div>
-              <form
-                action=""
-                className="flex items-center justify-center overflow-hidden"
-              >
-                <input
-                  className="text-xs font-normal outline-none w-80 placeholder:text-slate-400 pl-3 py-3"
-                  type="search"
-                  placeholder="Tìm kiếm sản phẩm"
-                />
-                <button className="p-4">
-                  <BsSearch />
-                </button>
-              </form>
+            <div className="invisible group-hover/item:visible -mt-3 pt-3 rounded-b-lg overflow-hidden absolute z-10 shadow-lg w-52 bg-white py-2">
+              {categories.map((value, index) => {
+                return (
+                  <a
+                    href="#"
+                    key={index}
+                    className="text-sm font-medium block px-3.5 py-1.5 hover:bg-[#ed1c24] hover:text-white"
+                  >
+                    {value}
+                  </a>
+                );
+              })}
             </div>
+          </div>
+          <form
+            action=""
+            className="flex items-center justify-center overflow-hidden"
+          >
+            <input
+              className="text-xs font-normal outline-none w-80 placeholder:text-slate-400 pl-3 py-3"
+              type="search"
+              placeholder="Tìm kiếm sản phẩm"
+            />
+            <button className="p-4">
+              <BsSearch />
+            </button>
+          </form>
+        </div>
 
-            <div className="grid gap-1 grid-cols-2 text-white">
-              <a href="#" className="flex justify-center leading-5 p-2">
-                <BsCartPlus className="text-xl mr-1" />
-                Giỏ hàng
-              </a>
-              <a href="#" className="flex justify-center leading-5 p-2">
-                <BsFillPersonFill className="text-xl mr-1" />
-                Hội viên
-              </a>
-            </div>
-          </nav>
-        </header>
-      </main>
-    </>
+        <div className="grid gap-1 grid-cols-2 text-white">
+          <a href="#" className="flex justify-center leading-5 p-2">
+            <BsCartPlus className="text-xl mr-1" />
+            Giỏ hàng
+          </a>
+          <a href="#" className="flex justify-center leading-5 p-2">
+            <BsFillPersonFill className="text-xl mr-1" />
+            Hội viên
+          </a>
+        </div>
+      </nav>
+    </header>
   );
 }
