63f7e8898995a8be02238b80fa4ae210a955a9cf
hieupt <trunghieupham03@gmail.com>
2023-04-24 20:05:44 +0700

    #

--- client/src/models/LoginResponse.ts
+++
@@ -1,6 +0,0 @@
-import { CustomerModel } from "./CustomerModel";
-
-export interface LoginResponse {
-    customer: CustomerModel;
-    token: String;
-}
--- client/src/services/IProductService.ts
+++
@@ -1,8 +0,0 @@
-import { ProductModel } from "@/models/ProductModel";
-
-export interface IProductService {
-    getAll(pageNum: Number, pageSize: Number): Promise<ProductModel>;
-    getById(id: Number): Promise<ProductModel>;
-    getByCategory(categoryId: Number, pageNum: Number, pageSize: Number): Promise<ProductModel[]>;
-    getProductImagePath(id: Number): string;
-}
