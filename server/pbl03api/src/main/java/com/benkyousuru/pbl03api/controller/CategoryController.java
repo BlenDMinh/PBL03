033f5f0266ad146b277b1e458ac574ce83575add
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2023-04-27 12:09:17 +0700

    CORS

--- server/pbl03api/src/main/java/com/benkyousuru/pbl03api/controller/CategoryController.java
+++ server/pbl03api/src/main/java/com/benkyousuru/pbl03api/controller/CategoryController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.benkyousuru.pbl03api.model.service.ICategoryService;
 import jakarta.transaction.Transactional;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class CategoryController {
     static final String basePath = "/api/category";
 
--- server/pbl03api/src/main/java/com/benkyousuru/pbl03api/controller/ProductController.java
+++ server/pbl03api/src/main/java/com/benkyousuru/pbl03api/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import com.benkyousuru.pbl03api.model.service.IProductService;
 import jakarta.transaction.Transactional;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProductController {
     static final String basePath = "/api/product";
 
