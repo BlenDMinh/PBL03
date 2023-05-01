a52f3a9f1e988dd38c77889c7546ac80cda9e058
hieupt <trunghieupham03@gmail.com>
2023-05-01 15:35:46 +0700

    #

--- vite-client/src/services/implement/CustomerService.ts
+++ vite-client/src/services/implement/CustomerService.ts
@@ -8,42 +8,57 @@ export class CustomerService implements ICustomerService {
   readonly baseUrl = "http://localhost:8080/api/customer";
 
   update(): Promise<void> {
-    if(this.loggedInCustomer == undefined)
-      return Promise.reject();
-    return http.put(this.baseUrl, new Headers(), JSON.stringify(this.loggedInCustomer));
+    if (this.loggedInCustomer == undefined) return Promise.reject();
+    return http.put(
+      this.baseUrl,
+      new Headers(),
+      JSON.stringify(this.loggedInCustomer)
+    );
   }
-  register(customer: Customer, password: string): Promise<void> {
-    return http.post<Customer>(this.baseUrl, new Headers(), JSON.stringify(customer))
-    .then((nCustomer) => {
-      this.loggedInCustomer = nCustomer;
-      this.changePassword(password);
-    });
+
+  async register(customer: Customer, password: string): Promise<void> {
+    const nCustomer = await http.post<Customer>(
+      this.baseUrl,
+      new Headers(),
+      JSON.stringify(customer)
+    );
+    this.loggedInCustomer = nCustomer;
+    this.changePassword(password);
   }
-  login(request: LoginRequest | undefined = undefined): Promise<LoginResponse> {
-    if(request == undefined) {
-      if(!localStorage.token)
-        return Promise.reject();
+
+  async login(
+    request: LoginRequest | undefined = undefined
+  ): Promise<LoginResponse> {
+    if (request == undefined) {
+      if (!localStorage.token) return Promise.reject();
       request = {
-        token: localStorage.token
+        token: localStorage.token,
       };
     }
-    return http.post<LoginResponse>(
+    const response = await http.post<LoginResponse>(
       this.baseUrl + "/login",
       new Headers(),
       JSON.stringify(request)
-    ).then((response) => {
-      this.loggedInCustomer = response.customer;
-      localStorage.token = response.token;
-      return response;
-    });
+    );
+    this.loggedInCustomer = response.customer;
+    localStorage.token = response.token;
+    return response;
   }
+
   logout(): Promise<void> {
     throw new Error("Method not implemented.");
   }
+
   changePassword(password: string): Promise<void> {
-    if(this.loggedInCustomer == undefined)
-      return Promise.reject();
-    return http.post(this.baseUrl + "/" + this.loggedInCustomer.customerId + "/change-password", new Headers(), password);
+    if (this.loggedInCustomer == undefined) return Promise.reject();
+    return http.post(
+      this.baseUrl +
+        "/" +
+        this.loggedInCustomer.customerId +
+        "/change-password",
+      new Headers(),
+      password
+    );
   }
 
   loggedInCustomer: Customer | undefined;
