2b7a8f423de0426d8bcf8a26641a547d9d8229e0
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2023-05-05 09:49:46 +0700

    Order Service

--- vite-client/src/services/IOrderService.ts
+++ vite-client/src/services/IOrderService.ts
@@ -1,3 +1,8 @@
+import { z } from "zod";
+import { Order, OrderSchema } from "../models/Order";
+
 export interface IOrderService {
-    
+    getById(id: Number): Promise<z.infer<typeof OrderSchema>>;
+    insert(order: Order): Promise<z.infer<typeof OrderSchema>>;
+    update(order: Order): Promise<z.infer<typeof OrderSchema>>;
 }
--- vite-client/src/services/implement/OrderService.ts
+++ vite-client/src/services/implement/OrderService.ts
@@ -1,7 +1,24 @@
+import { z } from "zod";
+import { Order, OrderSchema } from "../../models/Order";
 import { IOrderService } from "../IOrderService";
+import { http } from "../utils/http";
 
 export class OrderService implements IOrderService {
+    readonly baseUrl = "http://localhost:8080/api/order";
+    private static instance: OrderService;
     public static getInstance() {
-        
+        if(this.instance == undefined)
+            this.instance = new OrderService();
+        return this.instance;
+    }
+
+    getById(id: Number): Promise<z.infer<typeof OrderSchema>> {
+        return http.get(this.baseUrl + "/" + id);
+    }
+    insert(order: Order): Promise<z.infer<typeof OrderSchema>> {
+        return http.post(this.baseUrl, new Headers(), JSON.stringify(order));
+    }
+    update(order: Order): Promise<z.infer<typeof OrderSchema>> {
+        return http.put(this.baseUrl, new Headers(), JSON.stringify(order));
     }
 }
