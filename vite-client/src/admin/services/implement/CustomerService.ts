cfa4c8512104e31852e2c20705c859e48ca1d273
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2023-05-11 17:48:46 +0700

    Update CustomerService.ts

--- vite-client/src/admin/services/implement/CustomerService.ts
+++ vite-client/src/admin/services/implement/CustomerService.ts
@@ -1,22 +1,36 @@
+import { z } from "zod";
+import { Customer, CustomerSchema, CustomersSchema } from "../../../models/Customer";
 import { ICustomerService } from "../ICustomerService";
+import { http } from "../../../services/utils/http";
 
-class CustomerService implements ICustomerService {
+export class CustomerService implements ICustomerService {
+    private static instance: CustomerService = new CustomerService();
+
+    static getInstance(): CustomerService {
+        if(this.instance == undefined)
+            this.instance = new CustomerService();
+        return this.instance;
+    }
+    private constructor() {}
+
+    readonly baseUrl = "http://localhost:8080/api/customer";
+
     getAll(): Promise<z.infer<typeof CustomersSchema>> {
-
+        return http.get<z.infer<typeof CustomersSchema>>(this.baseUrl);
     }
     getById(id: number): Promise<z.infer<typeof CustomerSchema>> {
-
+        return http.get<z.infer<typeof CustomerSchema>>(this.baseUrl + "/" + id);
     }
-    insert(category: Customer): Promise<z.infer<typeof CustomerSchema>> {
-
+    insert(customer: Customer): Promise<z.infer<typeof CustomerSchema>> {
+        return http.post(this.baseUrl, new Headers(), JSON.stringify(customer));
     }
-    update(category: Customer): Promise<z.infer<typeof CustomerSchema>> {
-
+    update(customer: Customer): Promise<z.infer<typeof CustomerSchema>> {
+        return http.put(this.baseUrl, new Headers(), JSON.stringify(customer));
     }
-    delete(category: Customer): Promise<void> {
-
+    delete(customer: Customer): Promise<void> {
+        return http.delete(this.baseUrl, new Headers(), JSON.stringify(customer));
     }
     deleteById(id: number): Promise<void> {
-        
+        return http.delete(this.baseUrl + "/" + id);
     }
 }
