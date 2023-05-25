2a2d909430e0b44a935e10c7bf747de944344813
chouchou <chaudiemhoang2@gmail.com>
2023-05-25 15:38:35 +0700

    Update AdminMain.tsx

--- vite-client/src/pages/admin/AdminMain.tsx
+++ vite-client/src/pages/admin/AdminMain.tsx
@@ -1,10 +1,14 @@
 import { DollarSign, ShoppingBag, ShoppingCart } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import AdminNavbar from "../../components/admin/AdminNavbar";
 import { Customer } from "../../models/Customer";
 import { Order } from "../../models/Order";
 import { CustomerService } from "../../services/CustomerService";
 import { OrderService } from "../../services/admin/OrderService";
+import Chart from "chart.js";
+import React from "react";
+
+
 
 function AdminMain() {
   const [orders, setOrders] = useState<Order[]>([]);
@@ -60,11 +64,13 @@ function AdminMain() {
                 </div>
               </div>
             </div>
-            <div className="absolute top-[calc(670px)] bg-white rounded-xl w-10/12 h-96 shadow-2xl">
-              <span className="text-6xl text-slate-500 font-bold flex justify-center items-center h-full">
-                Web đang được xây dựng
-              </span>
+            
+            <div className="absolute top-[calc(670px)] bg-white rounded-xl w-10/12 h-200 shadow-2xl">
+                <canvas ref={sumPriceMonth(orders,2023,5)} />
             </div>
+            {/* <div className="absolute top-[calc(1500px)] bg-white rounded-xl w-10/12 h-200 shadow-2xl">
+
+            </div> */}
           </div>
         </div>
       </div>
@@ -91,5 +97,130 @@ function sumProductCount(orders: Order[]) {
   });
   return sum.toLocaleString();
 }
+
+function sumPriceYear(orders: Order[], year : number){
+  let ordersByMonth = Array(12).fill(0);
+  orders.forEach((order) => {
+  const date = new Date(order.dateCreated);
+  let monthIndex = date.getMonth();
+  let yearIndex = date.getFullYear();
+  if (order.products == undefined) return;
+  if (yearIndex == year)  
+    order.products.forEach((product) => {
+      ordersByMonth[monthIndex] += product.listedPrice;
+  });
+});
+  const chartRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (!chartRef.current) return;
+
+    const ctx = chartRef.current.getContext("2d");
+
+    if (!ctx) return;
+
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        datasets: [
+          {
+            label: "Tổng doanh thu theo tháng (VND)",
+            data: ordersByMonth,
+            backgroundColor: "rgba(54, 162, 235, 0.2)",
+            borderColor: "rgba(54, 162, 235, 1)",
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
+  }, [ordersByMonth]);
+    return chartRef;
+} 
+
+function sumPriceMonth(orders: Order[], year: number, month: number) {
+  let ordersByDay = Array(getDaysInMonth(year, month)).fill(0);
+
+  orders.forEach((order) => {
+    const date = new Date(order.dateCreated);
+    let dayIndex = date.getDate() - 1;
+    let monthIndex = date.getMonth() + 1;
+    let yearIndex = date.getFullYear();
+    if (order.products == undefined) return;
+    if (yearIndex == year && monthIndex == month) {
+      order.products.forEach((product) => {
+        ordersByDay[dayIndex] += product.listedPrice;
+      });
+    }
+  });
+
+  const chartRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (!chartRef.current) return;
+
+    const ctx = chartRef.current.getContext("2d");
+
+    if (!ctx) return;
+
+    new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: getDaysArray(year, month),
+        datasets: [
+          {
+            label: "Tổng doanh thu theo ngày (VND)",
+            data: ordersByDay,
+            fill: false,
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
+  }, [ordersByDay]);
+
+  return chartRef;
+}
+
+function getDaysInMonth(year: number, month: number) {
+  return new Date(year, month, 0).getDate();
+}
+
+function getDaysArray(year: number, month: number) {
+  const numDays = getDaysInMonth(year, month);
+  const daysArray = [];
+
+  for (let i = 1; i <= numDays; i++) {
+    daysArray.push(i.toString());
+  }
+
+  return daysArray;
+}
+
+
 
 export default AdminMain;
